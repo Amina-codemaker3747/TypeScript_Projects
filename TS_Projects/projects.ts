c1dfd06935399207c71b81bc2c221d09a6184af0
Amina Noor <159250991+Amina-codemaker3747@users.noreply.github.com>
2024-04-09 01:22:24 +0500

    Add files via upload

---
+++ TS_Projects/projects.ts
@@ -0,0 +1,63 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { ATM } from "./atm.js";
+import { adventureGame } from "./adventure.js";
+import { TODOS } from "./todo.js";
+import { wordCounter } from "./wordCounter.js";
+import { countTimer } from "./timer.js";
+import { stdManagement } from "./std.js";
+
+
+    // Main menu
+const mainMenu = async () => {
+    const { choose } = await inquirer.prompt({
+        type: 'list',
+        name: 'choose',
+        message: 'Select a game to play:',
+        choices: ['Adventure Game','ATM' , 'TODO List', 'CountDown Timer', 'Word Counter', 'Stuent Management System', 'Quit']
+    });
+
+   
+
+    switch (choose) {
+            
+        case 'Adventure Game':
+            await adventureGame();
+       break;
+
+       case 'ATM':
+        await ATM();
+        break;
+       
+       case 'TODO List':
+            await TODOS();
+       break;
+   
+       case 'CountDown Timer':
+           await countTimer();
+       break;
+   
+       case 'Word Counter':
+            await wordCounter();
+       break;
+   
+       case 'Student Management System':
+           await  stdManagement();
+       break;
+
+        case 'Quit':
+            console.log(chalk.bgCyanBright.redBright.italic('Goodbye!'));
+            process.exit(0);
+           
+    }
+};
+ 
+
+
+
+// Start the main menu
+mainMenu();
+
+
---
+++ TS_Projects/timer.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const countTimer = async () => {
+
+let num = await inquirer.prompt({
+    type: "number",
+    name: "User",
+    message: "Enter Your Amount"
+})
+
+const countDown = (second: number) => {
+    const interval = setInterval(() => {
+        console.log(`${second} Remaining seconds!`);
+        second--;
+
+        if(second < 0) {
+            clearInterval(interval);
+            console.log("CountDown Finished!");
+        }
+    }, 1000)
+}
+
+countDown(num.User);
+
+}
+
+export {countTimer}
